8758624d920a999ca3f45c707b0e7ca214c66848
Tim Pfeifer <timpfeifer@mailbox.org>
2019-05-02 12:22:52 +0200

    added timestamp to tables

--- sql/create_bookboxes.sql
+++ sql/create_bookboxes.sql
@@ -1,11 +1,13 @@
 CREATE TABLE BookBoxes
 (
     id          SERIAL PRIMARY KEY,
-    userId      INTEGER REFERENCES Users (id),
+    userid      INTEGER REFERENCES Users (id),
     description VARCHAR(255),
     location    VARCHAR(255),
     lat         DECIMAL(10, 8) NOT NULL,
     lng         DECIMAL(11, 8) NOT NULL,
     imgSrc      VARCHAR(255),
-    hint        TEXT
+    hint        TEXT,
+    created     date DEFAULT current_date,
+    updated     date
 )
--- sql/create_users.sql
+++ sql/create_users.sql
@@ -1,9 +1,12 @@
 CREATE TABLE Users
 (
-    id       SERIAL PRIMARY KEY,
-    username VARCHAR(20) UNIQUE NOT NULL,
-    password TEXT NOT NULL,
-    deleted BOOLEAN NOT NULL DEFAULT FALSE
+    id         SERIAL PRIMARY KEY,
+    username   VARCHAR(20) UNIQUE NOT NULL,
+    password   TEXT               NOT NULL,
+    deleted    BOOLEAN            NOT NULL DEFAULT FALSE,
+    created    date                        DEFAULT current_date,
+    updated    date,
+    last_login date
 )
 
 -- INSERT INTO users (email, password) VALUES (
