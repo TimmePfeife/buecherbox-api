3569dbc5a29c67821ade6ede7b44b6eb10d0a58d
Tim Pfeifer <timpfeifer@mailbox.org>
2019-05-02 12:22:52 +0200

    added timestamp to tables

--- sql/create_favorites.sql
+++ sql/create_favorites.sql
@@ -1,6 +1,7 @@
 CREATE TABLE Favorites
 (
-    id     SERIAL PRIMARY KEY,
-    userId INTEGER REFERENCES Users (id) NOT NULL,
-    bookboxId INTEGER REFERENCES BookBoxes (id) NOT NULL
+    id        SERIAL PRIMARY KEY,
+    userid    INTEGER REFERENCES Users (id)     NOT NULL,
+    bookboxid INTEGER REFERENCES BookBoxes (id) NOT NULL,
+    created   date DEFAULT current_date
 );
