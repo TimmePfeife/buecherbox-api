a20a444b7238d1185df47d327634fb464d1ac3f3
Tim Pfeifer <timpfeifer@mailbox.org>
2019-05-02 14:29:15 +0200

    insert images

--- sql/create_images.sql
+++ sql/create_images.sql
@@ -5,7 +5,7 @@ CREATE TABLE Images
     mimetype    VARCHAR(100)              NOT NULL,
     destination VARCHAR(260)              NOT NULL,
     path        VARCHAR(260)              NOT NULL,
+    thumbnail   VARCHAR(260)              NOT NULL,
     size        INT                       NOT NULL,
-    thumbnail   bytea,
     created     date DEFAULT current_date NOT NULL
 )
--- src/routes/index.js
+++ src/routes/index.js
@@ -3,6 +3,7 @@ const Log = require('../middleware/log');
 
 const UsersRoute = require('./users.route');
 const BookBoxRoute = require('./bookbox.route');
+const ImagesRoute = require('./images.route');
 
 const Router = Express.Router();
 
@@ -10,5 +11,6 @@ Router.use(Log);
 
 Router.use('/users', UsersRoute);
 Router.use('/bookboxes', BookBoxRoute);
+Router.use('/images', ImagesRoute);
 
 module.exports = Router;
