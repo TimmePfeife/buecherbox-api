443974030f98c327d216db37ec6ee91872afbf0e
Tim Pfeifer <timpfeifer@mailbox.org>
2019-05-04 01:22:45 +0200

    tests for bookbox

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,9 +1,35 @@
+const Fs = require('fs');
+const Path = require('path');
 const Pool = require('pg').Pool;
 
-module.exports = new Pool({
+const sql = {};
+const sqlDir = 'sql';
+const ignore = ['init_postgres.sql'];
+
+function readSql () {
+  const scripts = Fs.readdirSync(sqlDir);
+  for (let i = 0; i < scripts.length; i++) {
+    const sqlFile = scripts[i];
+
+    if (ignore.includes(sqlFile)) continue;
+
+    const path = Path.join(sqlDir, sqlFile);
+    sql[sqlFile] = Fs.readFileSync(path).toString();
+  }
+}
+
+const _pool = new Pool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB,
   host: process.env.DB_HOST,
   port: process.env.DB_PORT
 });
+
+readSql();
+
+_pool.sqlScripts = sql;
+_pool.sqlDir = sqlDir;
+_pool.sqlIgnore = ignore;
+
+module.exports = _pool;
--- test/resources/data.js
+++ test/resources/data.js
@@ -2,15 +2,37 @@ const Faker = require('faker');
 
 const entries = 10;
 
+const bookboxes = [];
 const users = [];
 
-function init () {
+async function init () {
+  initUsers();
+  await initBookboxes();
+}
+
+function initUsers () {
   for (let i = 0; i < entries; i++) {
     users.push({
       id: i + 1,
-      username: Faker.internet.userName(),
+      username: Faker.internet.userName().substring(0, 20),
       password: Faker.internet.password(),
       deleted: Faker.random.boolean()
+    });
+  }
+}
+
+async function initBookboxes () {
+  for (let i = 0; i < entries; i++) {
+    const creatorId = Math.floor(Math.random() * (entries - 1) + 1);
+
+    bookboxes.push({
+      userid: creatorId,
+      description: Faker.lorem.slug(),
+      location: '',
+      lat: parseFloat(Faker.address.latitude()),
+      lng: parseFloat(Faker.address.longitude()),
+      imgsrc: '',
+      hint: Faker.lorem.lines()
     });
   }
 }
@@ -18,5 +40,6 @@ function init () {
 module.exports = {
   init,
   entries,
-  users
+  users,
+  bookboxes
 };
