c211759a160224ae670fc182424ff2ce20172a82
Tim Pfeifer <timpfeifer@mailbox.org>
2019-05-13 08:38:48 +0200

    usernames/userids are case insensitive (OWASP recommendation)

--- sql/create_users.sql
+++ sql/create_users.sql
@@ -1,10 +1,10 @@
 CREATE TABLE Users
 (
     id         SERIAL PRIMARY KEY,
-    username   VARCHAR(20) UNIQUE NOT NULL,
-    password   TEXT               NOT NULL,
-    deleted    BOOLEAN            NOT NULL DEFAULT FALSE,
-    created    TIMESTAMP                   DEFAULT current_timestamp,
+    username   CITEXT UNIQUE NOT NULL,
+    password   TEXT          NOT NULL,
+    deleted    BOOLEAN       NOT NULL DEFAULT FALSE,
+    created    TIMESTAMP              DEFAULT current_timestamp,
     updated    TIMESTAMP,
     last_login TIMESTAMP
 );
