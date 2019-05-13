212a2cfa14c4c3c2524ede9c26848cda93576827
Tim Pfeifer <timpfeifer@mailbox.org>
2019-05-13 12:55:06 +0200

    use argon2 as password hashing algorithm

--- sql/init_postgres.sql
+++ sql/init_postgres.sql
@@ -7,6 +7,5 @@ GRANT ALL PRIVILEGES ON DATABASE buecherbox_db TO buecherbox;
 GRANT ALL PRIVILEGES ON DATABASE buecherbox_test_db TO buecherbox;
 
 -- needs to be enabled for buecherbox_db and buecherbox_test_db
-CREATE EXTENSION IF NOT EXISTS pgcrypto;
 CREATE EXTENSION IF NOT EXISTS citext;
 
